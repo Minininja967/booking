adef2befdd4385eee9c188c2d1846d2e0321fdb1
MALYKHDESKTOP\u6o00 <n.malykh@icvr.io>
2025-06-01 12:04:22 +0300

    Headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from playwright.sync_api import sync_playwright
 def page():
     """Запускаем браузер перед каждым тестом и закрываем после"""
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=500)
+        browser = p.chromium.launch(headless=True, slow_mo=500)
         context = browser.new_context(viewport={"width": 1920, "height": 1080})
         page = context.new_page()
         yield page
